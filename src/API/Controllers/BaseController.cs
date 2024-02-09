a5a15975257d01fc01b18cdea592ac492acd79fd
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-08 23:43:40 -0300

    structuring base controller

--- src/API/Controllers/BaseController.cs
+++ src/API/Controllers/BaseController.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace API.Controllers 
 {
     [ApiController]
     public abstract class APIControllerBase : ControllerBase 
     {
-        private IDbContextTransaction _transaction
+        private IDbContextTransaction _transaction;
         private readonly DbContext _context;
+
+        #region [ Protected Properties ]
+        protected Guid UserId { get; set; }
+        protected bool AuthenticatedUser{ get; set; }
+        #endregion
     }
 }
