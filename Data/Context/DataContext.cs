a7cd48bd510125463427fd626554ac34b2fb3389
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-13 13:50:55 -0300

    creating data context

--- Data/Context/DataContext.cs
+++ Data/Context/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Data.Context
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        
+        }
     }
 }
