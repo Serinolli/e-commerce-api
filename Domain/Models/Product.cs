6c4e404c23a501f2c5df33fddb246b955b46fdee
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-14 21:37:35 -0300

    Create Product.cs

---
+++ Domain/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace Domain.Models {
+    public class Product {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public int Stock { get; set; }        
+
+    }
+}
