7d62355eeba20c10f9f35029b6d1114fd62d5bb8
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-14 22:33:58 -0300

    creating entity class and using it in product

---
+++ Domain/Models/Entity.cs
@@ -0,0 +1,13 @@
+namespace Domain.Models {
+    public abstract class Entity {
+        public Guid Id { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid CreationUser { get; set; }
+        public DateTime LastUpdate { get; set; }
+        protected Entity() 
+        {
+            Active = true; 
+        }
+    }
+}
--- Domain/Models/Product.cs
+++ Domain/Models/Product.cs
@@ -1,11 +1,18 @@
 namespace Domain.Models {
-    public class Product {
-        public int ID { get; set; }
+    public class Product : Entity {
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
         public int Stock { get; set; }        
-
+        public Product(Guid id, string nome, string descricao, decimal preco, string categoria, int estoque)
+        {
+            Id = id;
+            Name = nome;
+            Description = descricao;
+            Price = preco;
+            Category = categoria;
+            Stock = estoque;
+        }
     }
 }
