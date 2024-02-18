0ae351a2249ac6e5dcb157182524aaa27141d6e6
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-18 15:17:19 -0300

    Update Product.cs

--- Domain/Models/Commerce/Product.cs
+++ Domain/Models/Commerce/Product.cs
@@ -1,9 +1,11 @@
 namespace Domain.Models.Commerce {
-    public class Product(string name, string description, decimal price, string category, int stock) : Entity {
+    public class Product(string name, string description, decimal price, string category, int stock, int condition, string? variationKey) : Entity {
         public string Name { get; set; } = name;
         public string Description { get; set; } = description;
         public decimal Price { get; set; } = price;
         public string Category { get; set; } = category;
         public int Stock { get; set; } = stock;        
+        public int Condition { get; set; } = stock;        
+        public string? VariationKey { get; set; } = variationKey;
     }
 }
