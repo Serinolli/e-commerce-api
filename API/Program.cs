78f62dde9958404b1459102ae1bb5b6fa24a0b94
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-25 14:34:31 -0300

    undoing startup changes to fix migration generation

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,30 @@
 using Data.Context;
 using Microsoft.EntityFrameworkCore;
-using API;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTransient<Startup>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
-app.UseRouting();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
