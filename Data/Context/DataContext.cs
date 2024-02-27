c53065b8271f956055505ed045d7f6378d2fc44d
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-26 23:13:36 -0300

    avoiding decimal silent truncating

--- Data/Context/DataContext.cs
+++ Data/Context/DataContext.cs
@@ -9,12 +9,23 @@ namespace Data.Context
         {
         
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+               entity.Property(e => e.Price)
+                    .HasColumnType("decimal(18,2)");
+            });
+        }
 
         #region Commerce
         public DbSet<Product> Product { get; set; }
         public DbSet<Cart> Cart { get; set; }
         public DbSet<CartItem> CartItem { get; set; }
         public DbSet<Category> Category { get; set; }
+
         #endregion
     }
 }
