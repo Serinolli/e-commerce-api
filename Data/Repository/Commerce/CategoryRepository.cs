86b680beaa04c8908bee4a49972a474ee4d0adf5
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-27 21:54:47 -0300

    creating generic repository and category repository

---
+++ Data/Repository/Commerce/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models.Commerce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository.Commerce
+{
+    public class CategoryRepository : BaseRepository<Category>
+    {
+
+    }
+}
