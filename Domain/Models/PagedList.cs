993685f5a00c9977ffb86db18624a04bcc8dc79c
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-27 22:24:39 -0300

    creating pagedlist model

---
+++ Domain/Models/PagedList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models
+{
+    public class PagedList<T> : List<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRecords { get; set; }
+
+        public PagedList(List<T> records, int currentPage, int pageSize)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalRecords = records.Count;
+            TotalPages = (int)Math.Ceiling(records.Count / (double)pageSize);
+            AddRange(records);
+        }
+
+        public PagedList() { }
+    }
+}
