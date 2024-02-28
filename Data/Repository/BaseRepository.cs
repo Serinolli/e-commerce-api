57c1fd6d93e6d961397b6a91578c34d6518c9d59
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-02-27 22:33:41 -0300

    Update BaseRepository.cs

--- Data/Repository/BaseRepository.cs
+++ Data/Repository/BaseRepository.cs
@@ -1,13 +1,64 @@
-﻿using System;
+﻿using Domain.Interfaces.Repository;
+using Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Repository
 {
-    public abstract class BaseRepository<TEntity>
+    public abstract class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
     {
+        public Task Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task AddList(List<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedList<TEntity>> GetAll(bool includeInactive = false, PaginationParameters? parameters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetById(Guid id, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByQuery(bool includeInactive, Expression<Func<TEntity, bool>> condition, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetByQuery(bool includeInactive, Expression<Func<TEntity, bool>> condition, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
