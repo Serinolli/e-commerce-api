20d2c60ac002fa4ef1f58c169d04cf73b227bb59
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-03-05 22:19:28 -0300

    Create DependencyInjectionConfig.cs

---
+++ API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(
+                this IServiceCollection services
+            )
+        {
+            //services.AddScoped<IRepository, Repository>;
+            return services;
+        }
+    }
+}
