400a0dd45e1726cac3113f4fa5bb97053c38cb20
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-03-07 23:34:59 -0300

    creating transaction methods

--- API/Controllers/ControllerBaseAPI.cs
+++ API/Controllers/ControllerBaseAPI.cs
@@ -1,9 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace API.Controllers
 {
     public abstract class ControllerBaseAPI: ControllerBase
     {
-        public ControllerBaseAPI() { }
+        private readonly DbContext _context;
+        private IDbContextTransaction? _transaction;
+        public ControllerBaseAPI(DbContext context) 
+        {
+            _context = context;
+        }
+
+        protected async Task BeginTransaction()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        protected async Task CommitTransaction()
+        {
+            await _transaction?.CommitAsync();
+        }
+
+        protected async Task ReverterTransacao()
+        {
+            await _transaction?.RollbackAsync();
+        }
     }
 }
