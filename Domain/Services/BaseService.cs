66ee1198884f5e3f68a01c85abdc4c5eb4f0b27b
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-03-09 16:07:32 -0300

    creating base service

---
+++ Domain/Services/BaseService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class BaseService
+    {
+        #region Properties
+        private readonly DbContext _context;
+        private IDbContextTransaction? _transaction;
+        #endregion
+
+        #region Constructors
+        public BaseService(DbContext context) 
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Methods
+        protected async Task BeginTransaction()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        protected async Task CommitTransaction()
+        {
+            await _transaction?.CommitAsync();
+        }
+
+        protected async Task ReverterTransacao()
+        {
+            await _transaction?.RollbackAsync();
+        }
+        #endregion
+    }
+}
