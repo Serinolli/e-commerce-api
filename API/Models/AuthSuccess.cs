e4fc74a7bf48314bf7759248b0348a4b881ffa51
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-03-24 21:51:37 -0300

    Create AuthSuccess.cs

---
+++ API/Models/AuthSuccess.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Models
+{
+    public class AuthSuccess
+    {
+        public string Token { get; set; }
+        public double ExpirationTime { get; set; }
+
+        public AuthSuccess(string token, double expirationTime) 
+        {
+            Token = token;
+            ExpirationTime = expirationTime;
+        }
+    }
+}
