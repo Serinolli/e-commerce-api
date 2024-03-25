24dd374b4eadd848603187053d004a9409666629
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-03-24 21:38:24 -0300

    Create Authentication.cs

---
+++ API/Models/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Authentication
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Authentication(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
