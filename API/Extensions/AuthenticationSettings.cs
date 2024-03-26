7c78202ae52d5b3ed559b1cb8a07dae3b2efe664
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-03-25 23:06:30 -0300

    Create AuthenticationSettings.cs

---
+++ API/Extensions/AuthenticationSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Extensions
+{
+    public class AuthenticationSettings
+    {
+        public string EncryptionKey { get; } = "WhisperingThunderstorms";
+        public int ExpirationTime { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+
+    }
+}
