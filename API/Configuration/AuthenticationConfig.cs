e7ef11203ad56ec6e882dafa0e35ca2f6353dec7
Rafael Serinolli <rafael.serinolli2@gmail.com>
2024-03-25 23:07:35 -0300

    Update AuthenticationConfig.cs

--- API/Configuration/AuthenticationConfig.cs
+++ API/Configuration/AuthenticationConfig.cs
@@ -1,6 +1,42 @@
-﻿namespace API.Configuration
+﻿using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Configuration
 {
-    public class AuthenticationConfig
+    public static class AuthenticationConfig
     {
+        public static IServiceCollection AddAuthenticationConfig(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var authSettingsSection = configuration.GetSection("AuthenticationSettings");
+            services.Configure<AuthenticationSettings>(authSettingsSection);
+
+            var authSettings = authSettingsSection.Get<AuthenticationSettings>();
+            var key = Encoding.ASCII.GetBytes(authSettings.EncryptionKey);
+           
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = authSettings.Audience,
+                    ValidIssuer = authSettings.Issuer
+                };
+            });
+
+            return services;
+        }
     }
 }
